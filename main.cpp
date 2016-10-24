7e48cf9d628a69ed39bd01a17fd969b6e1731660
Patrick Shaughnessy <g00223903@gmit.ie>
2016-10-24 21:41:03 +0100

    added swav function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
+//incldes
 #include "mbed.h"
 #include "stdio.h"
-#include <iostream>
 #include "math.h"
-
-using namespace std;
-const double PI =  3.1415926535897;
-float x[100];
-float li = 0.4166;
 
 //P wave and PR interval default settings
 float a_pwav=0.25; //P wave amplitude
@@ -37,59 +32,100 @@ float a_uwav=0.035; //U wave amplitude
 float d_uwav=0.0476; //U wave duration
 float t_uwav=0.433; //not adjustable in program
 
+//function declarations
+void genX(float);
+void pWav (float * x, float a_pwav,float d_pwav, float t_pwav, float li);
+void pWav (float * x, float a_swav,float d_swav, float t_swav, float li);
+
+//variables
+const double PI =  3.1415926535897;
+float x[100];
+float li = 0.4166;
 
 int main()
 {
-	while (true)
-	{
-		genX();
-	}
+	genX(*x);
 }
-
 
 
 void genX( float * x)
 {
-	int a;
-	for(a = 0; *x < 100; x++)
+	for(int a = 0; x < 100; x++)
 	{
+
 		x[a] = 0.60008 + (a * 0.008);
 	}
 	genX(x);
 }
 
 
-void pWav (float * x, float a_pwav, float t_pwav, float li)
+void pWav (float * x, float a_pwav,float d_pwav, float t_pwav, float li)
 {
 	float a = a_pwav;
 	float x1  = *x + t_pwav;
 	float b = (2*li)/d_pwav;
 	float harm1, pwav1, pwav;
-
 	float p1 = 1/li, p2 = 0;
-	int z;
-	for( z = 0; z < 100; z++)
+
+	for(int z = 0; z < 100; z++)
 	{
 		harm1=(((sin((PI/(2*b))*(b-(2*z))))/(b-(2*z))+(sin((PI/(2*b))*(b+(2*z))))/(b+(2*z)))*(2/PI))*cos((z*PI*x1)/li);
 		p2=p2+harm1;
 	}
+
 	pwav1=p1+p2;
 	pwav=(a*pwav1);
+	/* matlab code
+	function [pwav]=p_wav(x,a_pwav,d_pwav,t_pwav,li)
+	l=li;
+	a=a_pwav;
+	x=x+t_pwav;
+	b=(2*l)/d_pwav;
+	n=100;
+	p1=1/l;
+	p2=0;
+	for i = 1:n
+	    harm1=(((sin((PI/(2*b))*(b-(2*i))))/(b-(2*i))+(sin((PI/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/PI))*cos((i*PI*x)/l);
+	    p2=p2+harm1;
+	end
+	pwav1=p1+p2;
+	pwav=a*pwav1;*/
 }
 
 
+void sWav (float * x, float a_swav,float d_swav, float t_swav, float li)
+{
+	float x1=*x-t_swav;
+	float a=a_swav;
+	float b=(2*li)/d_swav;
+
+	float s1=(a/(2*b))*(2-b);
+	float s2=0;
+	double harm3, swav;
+	for(int n = 0; n < 100; n++)
+	{
+		harm3=(((2*b*a)/(n*n*PI*PI))*(1-cos(( *PI)/b)))*cos((n*PI*x1)/li);
+		s2=s2+harm3;
+
+	}
+
+	swav=-1*(s1+s2);
+
+
 /*
-function [pwav]=p_wav(x,a_pwav,d_pwav,t_pwav,li)
+ * Matlab code
+
+function [swav]=s_wav(x,a_swav,d_swav,t_swav,li)
 l=li;
-a=a_pwav;
-x=x+t_pwav;
-b=(2*l)/d_pwav;
+x=x-t_swav;
+a=a_swav;
+b=(2*l)/d_swav;
 n=100;
-p1=1/l;
-p2=0;
+s1=(a/(2*b))*(2-b);
+s2=0;
 for i = 1:n
-    harm1=(((sin((PI/(2*b))*(b-(2*i))))/(b-(2*i))+(sin((PI/(2*b))*(b+(2*i))))/(b+(2*i)))*(2/PI))*cos((i*PI*x)/l);
-    p2=p2+harm1;
+    harm3=(((2*b*a)/(i*i*pi*pi))*(1-cos(( *pi)/b)))*cos((i*pi*x)/l);
+    s2=s2+harm3;
 end
-pwav1=p1+p2;
-pwav=a*pwav1;*/
+swav=-1*(s1+s2);*/
+}
