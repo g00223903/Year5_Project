85b73f070adfd56e563749f68e3facfa46d4de57
Pat Shox <patrickshaughnessy88@gmail.com>
2016-10-24 16:18:33 +0100

    mBed example GPIO

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+#include "stdio.h"
+#include <iostream>
+
+using namespace std;
+DigitalOut gpo(D0);
+DigitalOut led(LED_RED);
+
+int main()
+{
+
+    while (true)
+    {
+    	int count;
+        gpo = !gpo; // toggle pin
+        led = !led; // toggle led
+        wait(0.5f);
+        //cout << "Count: %d \r" << count;
+        printf("Count: %d\r\t",count);
+        count++;
+    }
+}
+
