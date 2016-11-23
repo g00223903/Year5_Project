bb931554cb9a410b12eaed283d07a51085742efa
Patrick Shaughnessy <g00223903@gmit.ie>
2016-11-23 21:38:18 +0000

    Initial Commit

---
+++ ecgSignalCcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ecgSignalCcode
+//
+//  Created by patrick shaughnessy on 23/11/2016.
+//  Copyright © 2016 patrick shaughnessy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
