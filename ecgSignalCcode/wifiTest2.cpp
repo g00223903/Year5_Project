2e63b07cba1699e79bcc3885f066687446e091e7
Patrick Shaughnessy <g00223903@gmit.ie>
2017-03-07 12:05:39 +0000

    tidying up code

--- ecgSignalCcode/wifiTest2.cpp
+++ ecgSignalCcode/wifiTest2.cpp
@@ -1,3 +1,4 @@
+
 //
 //  wifiTest2.cpp
 //  ecgSignalCcode
@@ -23,7 +24,9 @@ InterruptIn pir(SW2);
 Semaphore updates(0);
 
 SimpleResourceString name = client.define_resource("device/0/name", "jan", M2MBase::GET_PUT_ALLOWED, true);
-SimpleResourceInt myCount = client.define_resource("my/0/count", 0, M2MBase::GET_ALLOWED);
+SimpleResourceInt heartRate = client.define_resource("my/0/patientHeartRate", 0, M2MBase::GET_ALLOWED);
+//SimpleResourceInt myCount = client.define_resource("my/0/count", 0, M2MBase::GET_ALLOWED);
+
 
 // Registered callback for mbed Client
 DigitalOut statusLed(LED1, 1);
@@ -33,43 +36,83 @@ void registered() {
 
 
 
-int main(int, char**) {
-    //name = "Patrick Shaughnessy";
-    //ask for ssid and password
-    
-    char *ptr = "Patrick Shaughnessy";
-    name = ptr;
-    
-    
-    int numArray[100];
-    for(int x = 0; x < 100; x++)
+AnalogIn   ain(A0);
+static DigitalOut led(LED1);;
+Timer timer;
+int start=0, end, heartBeats = 0;
+int oldStartTime = 0, newStartTime = 0;
+float rate = 0.0;
+float peakMax = 0;
+volatile float temp = 0;
+float interval = 0;
+void  getHeartRate();
+int fPeakFlag = 0;
+int ary[100];
+int z = 0;
+
+float oldValue = 0, newValue = 0;
+void getHeartRate();
+void getHeartRate()
+{
+    printf("\033[H\033[J");//clears screen
+    printf("Get Heart Rate Function!!!\r\n");
+    led = 0;
+    timer.start();
+    while(1)
     {
-        numArray[x] = x;
+        oldValue = newValue;
+        newValue = ain.read();
+        if((newValue - oldValue > 0) && (newValue > 0.5f) && (fPeakFlag == 0))
+        {
+            oldStartTime = newStartTime;
+            newStartTime = timer.read_ms();
+            fPeakFlag = 1;
+            rate = ((60 / (float)(newStartTime - oldStartTime))*1000);
+            printf("Time: %d HeartRate %f \r\n",newStartTime-oldStartTime, rate);
+            heartRate = rate;
+        }
+        if(newValue < 0.5f)
+        {
+            fPeakFlag = 0;
+        }
     }
-    
-    myCount = numArray[51];
-    
+}
+
+void connect();
+void connect()
+{
     NetworkInterface* network = easy_connect(true);
     if (!network) {
         printf("Connect to internet failed... See serial output.\n");
-        return 1;
     }
-    
     struct MbedClientOptions options = client.get_default_options();
     options.DeviceType = "wifi test";
     if (!client.setup(options, network)) {
         printf("Setting up mbed_client failed...\n");
-        return 1;
     }
-    
     client.on_registered(&registered);
-    int count;
-    while (1) {
-        int v = updates.wait(25000);
-        if (v == 1) {
-            count +=1;
-            printf("\nCount: %i",count);
-        }
-        client.keep_alive();
-    }
+    
 }
+
+int main(int, char**) {
+    char *ptr = "Patrick Shaughnessy";
+    name = ptr;
+    //heartRate = 0;
+    connect();
+    getHeartRate();
+}
+
+
+/*    NetworkInterface* network = easy_connect(true);
+ if (!network) {
+ printf("Connect to internet failed... See serial output.\n");
+ return 1;
+ }
+ struct MbedClientOptions options = client.get_default_options();
+ options.DeviceType = "wifi test";
+ if (!client.setup(options, network)) {
+ printf("Setting up mbed_client failed...\n");
+ return 1;
+ }
+ client.on_registered(&registered);
+ */
